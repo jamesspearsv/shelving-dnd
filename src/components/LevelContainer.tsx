1fe65978223d2807f9f028e8faad368e1000d22e
James Spears <jamesspearsv@gmail.com>
2025-07-17 21:34:47 -0400

    Add level checking, completion trigger

--- src/components/LevelContainer.tsx
+++ src/components/LevelContainer.tsx
@@ -2,7 +2,7 @@ import { DndContext, type DragEndEvent } from '@dnd-kit/core';
 import type { Book } from '../App';
 import Draggable from './Draggable';
 import Droppable from './Droppable';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import styles from './LevelContainer.module.css';
 
 interface LevelContainerProps {
@@ -11,15 +11,38 @@ interface LevelContainerProps {
 }
 
 export default function LevelContainer(props: LevelContainerProps) {
-  const [list, setList] = useState(
-    props.level.sort(() => Math.random() - Math.random())
-  );
+  const modalRef = useRef<HTMLDialogElement>(null);
+  const [correctItems, setCorrectItems] = useState(0);
+  const [list, setList] = useState(() => {
+    return [...props.level].sort(
+      () => Math.random() - Math.random() + Math.random() * 0.1
+    );
+  });
+
+  useEffect(() => {
+    let c = 0;
+    list.forEach((item, index) => {
+      if (item === props.level[index]) {
+        c++;
+      }
+
+      setCorrectItems(c);
+
+      if (c !== list.length) return;
+      setTimeout(() => {
+        if (!modalRef.current) return;
+        const modal = modalRef.current;
+        modal.showModal();
+      }, 500);
+    });
+  }, [list, props.level]);
 
   function handleDragEnd(event: DragEndEvent) {
+    if (correctItems === list.length) return;
     if (event.over) {
       // console.log(event);
-      console.log('dragged item: ', list[event.active.id as number]);
-      console.log('dropped on: ', list[event.over.id as number]);
+      // console.log('dragged item: ', list[event.active.id as number]);
+      // console.log('dropped on: ', list[event.over.id as number]);
 
       const newList = [...list];
       // remove dragged item from list
@@ -34,11 +57,20 @@ export default function LevelContainer(props: LevelContainerProps) {
       setList(newList);
     }
   }
+
   return (
     <section>
       <section className={styles.heading}>
         <h1>{props.levelName}</h1>
-        <button>Check Solution</button>
+        <p
+          className={
+            correctItems === list.length
+              ? styles.levelCompleted
+              : styles.levelInProgress
+          }
+        >
+          Books shelved correctly: {correctItems} of {list.length}
+        </p>
       </section>
       <DndContext onDragEnd={handleDragEnd}>
         <div className={styles.level}>
@@ -62,6 +94,11 @@ export default function LevelContainer(props: LevelContainerProps) {
           ))}
         </div>
       </DndContext>
+      <dialog ref={modalRef}>
+        <article>
+          <h2>Level Completed!</h2>
+        </article>
+      </dialog>
     </section>
   );
 }
